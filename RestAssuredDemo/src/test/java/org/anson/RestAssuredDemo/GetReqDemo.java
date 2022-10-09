5bb99edf2ad2c3ba5bc7881eb10936ae5af7e957
anson-automate <anson.paul.p@gmail.com>
2022-10-09 11:12:55 +0530

    Get , base URI , log all , param

--- RestAssuredDemo/src/test/java/org/anson/RestAssuredDemo/GetReqDemo.java
+++ RestAssuredDemo/src/test/java/org/anson/RestAssuredDemo/GetReqDemo.java
@@ -1,24 +1,32 @@
 package org.anson.RestAssuredDemo;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+
 import java.util.List;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class GetReqDemo {
-
+	
+	
 	@Test
 	void TestStatusCode() {
 
-		RequestSpecification req = RestAssured.given();
+		RequestSpecification req = given().when();
 		Response res = req.get("https://reqres.in/api/users?page=2");
 
 		Assert.assertEquals(res.getStatusCode(), 200);
+		
+		//res.then().body("data[0].id",equalTo(6));
+		
+		res.then().body("data.id", hasItems(7,8));
 
 		JsonPath js = res.getBody().jsonPath();
 		
@@ -29,13 +37,16 @@ public class GetReqDemo {
 		
 		for (int i = 0; i< li.size();i++) {
 			System.out.println("index "+i+" "+js.get("data["+i+"].id"));
-//			System.out.println(js.get("data[0].email"));
-//			System.out.println(js.get("data[0].first_name"));
-//			System.out.println(js.get("data[0].last_name"));
-//			System.out.println(js.get("data[0].avatar"));
 		}
 
 
+	}
+	
+	@Test
+	void test2 () {
+		// base uri , param , log all
+		baseURI = "https://reqres.in/api";
+		given().header("content-type","application/json").param("page", 2).when().get("/users").then().log().all();
 	}
 
 }
